86fc2b33b530598df46f7079df75b5d3af71f617
Supawish Hanmontree <supawish35@gmail.com>
2025-11-19 12:15:27 +0700

    New Version

--- ConsoleEIE/Program.cs
+++ ConsoleEIE/Program.cs
@@ -2,23 +2,35 @@
 
 class Program {
     static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
-        Console.Write("Please enter your name(Eng) : ");
+        String name, gender;
+        int height;
+        
+        do {
+            Console.Clear();
+            //? Input Name
+            do {
+                Console.Write("Please enter your name(Eng) : ");
+                name = Console.ReadLine();
+            } while (name == "");
 
-        String name = Console.ReadLine();
+            Console.WriteLine("Hello, Welcome " + name);
 
-        Console.WriteLine("Hello, Welcome " + name);
+            //? Input Gender 
+            do {
+                Console.Write("Please enter your gender (M/W): ");
+                gender = Console.ReadLine().ToUpper();
+            } while (gender != "M" && gender != "W");
 
-        Console.Write("Please enter your gender (M/W): ");
-        String gender = Console.ReadLine().ToUpper();
-        Console.Write("Please enter your height (cm) : ");
+            //? Input 
+            do {
+                Console.Write("Please enter your height (cm) : ");
+            } while (!int.TryParse(Console.ReadLine(), out height));
 
-        int height = Convert.ToInt16(Console.ReadLine());
-        int weight = (gender == "M") ? height - 100 : (gender == "W") ? height - 110 : 0;
+            int weight = (gender == "M") ? height - 100 : (gender == "W") ? height - 110 : 0;
 
-        Console.WriteLine($"Your ideal weight is {weight.ToString()} kg");
-
-        Console.WriteLine("Press any key to close program");
-        Console.ReadKey();
+            Console.WriteLine($"Your ideal weight is {weight.ToString()} kg \n");
+            
+            Console.WriteLine("Press Q to exit program Or press any other key to continue...");
+        }while(Console.ReadKey().Key != ConsoleKey.Q);
     }
 }
