f53e01924f585bf7570e4c48ae417759693d5bb3
Supawish Hanmontree <supawish35@gmail.com>
2025-11-19 09:28:48 +0700

    Initial commit

---
+++ ConsoleEIE/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleEIE;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("My name is Brian ");
+        Console.WriteLine("Computer Science Program");
+        Console.WriteLine("-------------------------------------------------------------------------");
+        Console.Write("My Hometown is");
+        Console.Write("Buengkan");
+        Console.Write("Thailand");
+    }
+}
