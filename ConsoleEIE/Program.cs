b26db6d2269b5a8db4802c635b6c7f76f20eb129
Supawish Hanmontree <supawish35@gmail.com>
2025-11-19 11:31:50 +0700

    EIEI

--- ConsoleEIE/Program.cs
+++ ConsoleEIE/Program.cs
@@ -3,19 +3,22 @@
 class Program {
     static void Main(string[] args) {
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("Please enter your name(Eng) : ");
+        Console.Write("Please enter your name(Eng) : ");
 
         String name = Console.ReadLine();
 
         Console.WriteLine("Hello, Welcome " + name);
 
-        Console.WriteLine("Please enter your gender (M/W): ");
+        Console.Write("Please enter your gender (M/W): ");
         String gender = Console.ReadLine().ToUpper();
-        Console.WriteLine("Please enter your height (cm) : ");
+        Console.Write("Please enter your height (cm) : ");
 
         int height = Convert.ToInt16(Console.ReadLine());
         int weight = (gender == "M") ? height - 100 : (gender == "W") ? height - 110 : 0;
 
         Console.WriteLine($"Your ideal weight is {weight.ToString()} kg");
+
+        Console.WriteLine("Press any key to close program");
+        Console.ReadKey();
     }
 }
